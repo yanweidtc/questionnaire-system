d9971bab53e09cf690e7830767186b0eb8588958
Wei Yan <yanwei@youhaodongxi.com>
2025-05-26 14:53:04 +0800

    Docker setting completed

---
+++ admin-panel/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-config-provider>
+    <router-view></router-view>
+  </a-config-provider>
+</template>
+
+<script setup lang="ts">
+// 组件逻辑
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100vh;
+}
+</style>
